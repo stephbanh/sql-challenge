501dc584413b43b80c83514b606e85e9e74d682f
Stephanie Banh <steph33banh@gmail.com>
2022-02-19 16:13:41 -0800

    Add files via upload

---
+++ Queries.sql
@@ -0,0 +1,60 @@
+﻿--Run each query seperately as pg4Admin can't handle them all at once
+
+--List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT ei.emp_no, ei.last_name, ei.first_name, ei.sex, s.salary
+FROM employee_info as ei
+INNER JOIN salaries as s
+ON ei.emp_no = s.emp_no;
+
+--List first name, last name, and hire date for employees who were hired in 1986.
+SELECT first_name, last_name, hire_date 
+FROM employee_info
+WHERE hire_date BETWEEN '1986-01-01' AND '1987-01-01';
+
+--List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT d.dept_no, d.dept_name, dm.emp_no, ei.last_name, ei.first_name
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no
+INNER JOIN employee_info as ei
+ON dm.emp_no = ei.emp_no;
+
+--List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT de.emp_no, ei.last_name, ei.first_name, d.dept_name
+FROM dept_emp as de
+INNER JOIN employee_info as ei
+ON de.emp_no = ei.emp_no
+JOIN departments as d
+ON de.dept_no = d.dept_no;
+
+--List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT de.emp_no, ei.last_name, ei.first_name, d.dept_name
+FROM dept_emp as de
+INNER JOIN employee_info as ei
+ON de.emp_no = ei.emp_no
+INNER JOIN departments as d
+ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales';
+
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT de.emp_no, ei.last_name, ei.first_name, d.dept_name
+FROM dept_emp as de
+INNER JOIN employee_info as ei
+ON de.emp_no = ei.emp_no
+INNER JOIN departments as d
+ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales'
+OR d.dept_name = 'Development';
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name,
+COUNT(last_name) AS "frequency count"
+FROM employee_info
+GROUP BY last_name
+ORDER BY COUNT(last_name) DESC;
---
+++ Setup.sql
@@ -0,0 +1,133 @@
+﻿
+
+-- DROP DATABASE IF EXISTS employees;
+
+
+--if necessary for testing
+--CREATE DATABASE employees
+    --WITH 
+    --OWNER = postgres
+    --ENCODING = 'UTF8'
+    --LC_COLLATE = 'English_United States.1252'
+    --LC_CTYPE = 'English_United States.1252'
+    --TABLESPACE = pg_default
+    --CONNECTION LIMIT = -1;
+
+--privledges were added due to file permission issues
+--ALTER DEFAULT PRIVILEGES
+--GRANT ALL ON TABLES TO pg_database_owner;
+
+--ALTER DEFAULT PRIVILEGES
+--GRANT ALL ON TABLES TO postgres;
+
+-- if necessary for testing
+-- cascade needed to clear away keys 
+DROP TABLE IF EXISTS departments cascade;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS employee_info cascade;
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS titles;
+
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+CREATE TABLE "departments" (
+    "dept_no" text   NOT NULL,
+    "dept_name" text   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" int   NOT NULL,
+    "dept_no" text   NOT NULL
+);
+
+CREATE TABLE "employee_info" (
+    "emp_no" int   NOT NULL,
+    "emp_title" text NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" text   NOT NULL,
+    "last_name" text   NOT NULL,
+    "sex" text   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_employee_info" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" text   NOT NULL,
+    "emp_no" int   NOT NULL
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL
+);
+
+CREATE TABLE "titles" (
+    "title_id" text   NOT NULL,
+    "title" text   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+-- a way to do the imports manually 
+-- pg admin does allow for this with its own features but for the sake of showing how to import, this was included
+-- this also must be done before any foreign keys, else there will be issues
+
+COPY departments
+FROM 'C:\Users\steph\DataClass\UCI-VIRT-DATA-PT-11-2021-U-C\Homework\09-SQL\Instructions\data\departments.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY dept_emp
+FROM 'C:\Users\steph\DataClass\UCI-VIRT-DATA-PT-11-2021-U-C\Homework\09-SQL\Instructions\data\dept_emp.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY dept_manager
+FROM 'C:\Users\steph\DataClass\UCI-VIRT-DATA-PT-11-2021-U-C\Homework\09-SQL\Instructions\data\dept_manager.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY employee_info
+FROM 'C:\Users\steph\DataClass\UCI-VIRT-DATA-PT-11-2021-U-C\Homework\09-SQL\Instructions\data\employees.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY salaries
+FROM 'C:\Users\steph\DataClass\UCI-VIRT-DATA-PT-11-2021-U-C\Homework\09-SQL\Instructions\data\salaries.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY titles
+FROM 'C:\Users\steph\DataClass\UCI-VIRT-DATA-PT-11-2021-U-C\Homework\09-SQL\Instructions\data\titles.csv'
+DELIMITER ','
+CSV HEADER;
+
+-- foreign keys are added here to allow for the import
+--these were also taken from QuickDBD
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employee_info" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employee_info" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employee_info" ("emp_no");
+
+ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employee_info" ("emp_no");
+
+
+
+
